458d7bce861b332aca6728faf3b9fed32bea0ba2
Wojciech Szoda <wojciechszoda@icloud.com>
2023-10-06 06:03:32 +0200

    added configuration file

--- ClientCredentailsAuthorization.cs
+++ ClientCredentailsAuthorization.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using Microsoft.Extensions.Configuration;
 
 namespace ShiraSodaBot
 {
@@ -10,10 +8,19 @@ namespace ShiraSodaBot
     {
         private HttpClient _client = new HttpClient();
 
+        private readonly IConfiguration _config;
+
+        public ClientCredentailsAuthorization(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public async Task<string> Authorize()
         {
-            string client_id = "i5sc9akf9nb3mjrqrbd07bafeqhi9l";
-            string client_secret = "p04n1geikogpu3ibutxpjik1caz7kq";
+            string client_id = _config["ApiSettings:client_id"];
+            Console.WriteLine(client_id);
+            string client_secret = _config["ApiSettings:secret_id"];
+            Console.WriteLine(client_secret);
             string resultUrl = $"https://id.twitch.tv/oauth2/token?client_id={client_id}&client_secret={client_secret}&grant_type=client_credentials";
             HttpResponseMessage response = await _client.PostAsync(resultUrl, null);
             if (response.IsSuccessStatusCode)
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ShiraSodaBot;
+using Microsoft.Extensions.Configuration;
 using System.Data;
 using System.Net.Http;
 using System.Text;
@@ -8,9 +9,15 @@ namespace ShiraMelonSoda
 {
     public class Program
     {
-        void Main(string[] args)
+        static void Main(string[] args)
         {
-            IAuthorizationHandler authorizationHandler = new ClientCredentailsAuthorization();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            IAuthorizationHandler authorizationHandler = new ClientCredentailsAuthorization(configuration);
+            authorizationHandler.Authorize();
             Console.WriteLine("JD");
         }
     }
